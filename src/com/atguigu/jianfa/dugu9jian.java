a467fd457a033c87a1716e9b5b08df0c3292430d
Spiritlitao <spirit_litao@126.com>
2017-08-17 20:13:53 +0800

    修改

--- src/com/atguigu/jianfa/dugu9jian.java
+++ src/com/atguigu/jianfa/dugu9jian.java
@@ -3,5 +3,10 @@ package com.atguigu.jianfa;
 public class dugu9jian {
 	public static void main(String[] args) {
 		System.out.println("hello dugu9jian");
+		System.out.println("hello dugu9jian");
+		System.out.println("hello dugu9jian");
+		System.out.println("hello dugu9jian");
+		System.out.println("hello dugu9jian");
+		System.out.println("hello dugu9jian");
 	}
 }
